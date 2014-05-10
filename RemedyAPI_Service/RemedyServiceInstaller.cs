bde5c978b183eb2eb2994404d640375d5a0a1f55
atkinchris <atkinchris@gmail.com>
2014-05-10 23:59:52 +0100

    Added simple windows service of example

---
+++ RemedyAPI_Service/RemedyServiceInstaller.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel;
+using System.Configuration.Install;
+using System.ServiceProcess;
+
+namespace RemedyAPI_Service {
+
+    [RunInstaller( true )]
+    public class RemedyServiceInstaller : Installer {
+
+        private ServiceProcessInstaller processInstaller;
+        private ServiceInstaller serviceInstaller;
+
+        public RemedyServiceInstaller() {
+            processInstaller = new ServiceProcessInstaller();
+            serviceInstaller = new ServiceInstaller();
+
+            processInstaller.Account = ServiceAccount.LocalSystem;
+            serviceInstaller.StartType = ServiceStartMode.Manual;
+            serviceInstaller.ServiceName = "Remedy API Service";
+
+            Installers.Add( serviceInstaller );
+            Installers.Add( processInstaller );
+        }
+    }
+}
