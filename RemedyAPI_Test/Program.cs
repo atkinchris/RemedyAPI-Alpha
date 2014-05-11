c77e818c06943d11dbb97a33c1cd2f53fe73a9b4
atkinchris <atkinchris@gmail.com>
2014-05-11 21:16:32 +0100

    Added test project for immediate work

---
+++ RemedyAPI_Test/Program.cs
@@ -0,0 +1,19 @@
+﻿using RemedyAPI;
+using System;
+
+namespace RemedyAPI_Test {
+    class Program {
+        static void Main() {
+
+            var server = new Server( "chris", "atkin" ) {
+                cacheTime = 5
+            };
+            var query = new Query( null, "chris" );
+            for ( int i = 0; i < 100; i++ ) {
+                server.ExecuteQuery( query );
+                Console.WriteLine( query.results["INC000000160"].Submitted );
+                Console.ReadLine();
+            }
+        }
+    }
+}
