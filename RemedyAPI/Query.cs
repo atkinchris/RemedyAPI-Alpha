d8bef8167e92204cee17ce0cae2cd7a1e50c45c2
atkinchris <atkinchris@gmail.com>
2014-05-11 14:59:24 +0100

    Added static methods for common queries

--- RemedyAPI/Query.cs
+++ RemedyAPI/Query.cs
@@ -32,8 +32,53 @@ namespace RemedyAPI {
                 types.ToQuery(),
                 status.ToQuery(),
                 qualification
-            };            
+            };
             return String.Join( " AND ", parts.Where( p => p.IsNullOrBlank() == false ).Select( p => String.Format( "({0})", p ) ) );
+        }
+
+        static public int GetGroupDepth( Server server, string group ) {
+            var query = new Query( null, group );
+            server.ExecuteQuery( query );
+            return query.results.Count;
+        }
+        static public int GetGroupDepth( Server server, string[] groups ) {
+            var query = new Query( null, groups );
+            server.ExecuteQuery( query );
+            return query.results.Count;
+        }
+        static public Results GetGroupStack( Server server, string group ) {
+            var query = new Query( null, group );
+            server.ExecuteQuery( query );
+            return query.results;
+        }
+        static public Results GetGroupStack( Server server, string[] groups ) {
+            var query = new Query( null, groups );
+            server.ExecuteQuery( query );
+            return query.results;
+        }
+        static public int GetUserDepth( Server server, string user ) {
+            var query = new Query();
+            query.users.Add( user );
+            server.ExecuteQuery( query );
+            return query.results.Count;
+        }
+        static public int GetUserDepth( Server server, string[] users ) {
+            var query = new Query();
+            query.users.Add( users );
+            server.ExecuteQuery( query );
+            return query.results.Count;
+        }
+        static public Results GetUserStack( Server server, string user ) {
+            var query = new Query();
+            query.users.Add( user );
+            server.ExecuteQuery( query );
+            return query.results;
+        }
+        static public Results GetUserStack( Server server, string[] users ) {
+            var query = new Query();
+            query.users.Add( users );
+            server.ExecuteQuery( query );
+            return query.results;
         }
     }
 }
