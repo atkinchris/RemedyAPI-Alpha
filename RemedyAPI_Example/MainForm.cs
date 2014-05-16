2e0b2c4bdb50497d12e8f7ff5177020e9de65765
Chris Atkin <atkinchris@gmail.com>
2014-05-16 16:15:11 +0100

    Removed CSV code

--- RemedyAPI_Example/MainForm.cs
+++ RemedyAPI_Example/MainForm.cs
@@ -16,7 +16,7 @@ namespace RemedyAPI_Example {
         private Server _remedy;
         private readonly BackgroundWorker _bw = new BackgroundWorker();
         private readonly Timer _timer = new Timer();
-        private string _resultsPath;
+        private string _path;
         private Database _db = new Database();
 
         public MainForm() {
@@ -37,14 +37,14 @@ namespace RemedyAPI_Example {
                 var identity = windowsIdentity.Name;
                 usernameInput.Text = identity.Substring( identity.IndexOf( @"\", StringComparison.Ordinal ) + 1 );
             }
-            outputPathText.Text = @"Z:\stats\results.csv";
+            outputPathText.Text = @"Z:\stats\";
         }
 
         private void Start( object sender, EventArgs e ) {
 
             var username = usernameInput.Text;
             var password = passwordInput.Text;
-            _resultsPath = outputPathText.Text;
+            _path = outputPathText.Text;
 
             if ( string.IsNullOrEmpty( username ) ) {
                 MessageBox.Show( "Invalid username", "Oops", MessageBoxButtons.OK, MessageBoxIcon.Error );
@@ -84,10 +84,9 @@ namespace RemedyAPI_Example {
         }
 
         private void ToggleRefresh( bool start ) {
-
             usernameInput.Enabled = !start;
             passwordInput.Enabled = !start;
-            browseButton.Enabled = !start;
+            outputPathText.Enabled = !start;
             startButton.Text = start ? "Stop" : "Start";
             if ( start ) {
                 startButton.Click -= Start;
@@ -124,25 +123,6 @@ namespace RemedyAPI_Example {
             statusLabel.Text = "Ready.";
             var results = _queries.GetResultsCount();
 
-            if ( !File.Exists( _resultsPath ) ) {
-                var titles = new StringBuilder();
-                titles.AppendFormat( "{0}", "Timecode" );
-                foreach ( var title in results.Keys ) {
-                    titles.AppendFormat( ",{0}", title );
-                }
-                titles.AppendLine();
-                File.WriteAllText( _resultsPath, titles.ToString() );
-            }
-
-            var output = new StringBuilder();
-            output.Append( DateTime.Now );
-            foreach ( var result in results.Values ) {
-                output.AppendFormat( ",{0}", result );
-            }
-            using ( StreamWriter sw = File.AppendText( _resultsPath ) ) {
-                sw.WriteLine( output.ToString() );
-            }
-
             _db.Add( results[ "Outstanding" ], results[ "Submitted" ], results[ "Resolved" ] );
             var dbResults = _db.GetData( 60 );
 
@@ -150,26 +130,14 @@ namespace RemedyAPI_Example {
             flotData.Add(new FlotSeries( "Outstanding", dbResults, 1 ));
             flotData.Add(new FlotSeries( "Submitted", dbResults, 2 ));
             flotData.Add(new FlotSeries( "Resolved", dbResults, 3 ));
-            JSON.WritetoFile( flotData, @"results.json" );
-        }
-
-        private void browseButton_Click( object sender, EventArgs e ) {
-            var dialog = new SaveFileDialog { FileName = "results.csv" };
-            if ( dialog.ShowDialog( this ) == DialogResult.OK ) {
-                outputPathText.Text = dialog.InitialDirectory + dialog.FileName;
-            }
+            JSON.WritetoFile( flotData, _path + "results.json" );
         }
 
         private void notifyIcon1_DoubleClick( object sender, EventArgs e ) {
-            // Show the form when the user double clicks on the notify icon.
             Show();
-
-            // Set the WindowState to normal if the form is minimized.
             if ( WindowState == FormWindowState.Minimized ) {
                 WindowState = FormWindowState.Normal;
             }
-
-            // Activate the form.
             Activate();
         }
 
