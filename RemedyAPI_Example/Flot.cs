1241c0ce30b05ed010f19d2cd4cb1b7e13ea9d5b
Chris Atkin <atkinchris@gmail.com>
2014-05-19 16:26:59 +0100

    Fixed case in data, label and color field names

--- RemedyAPI_Example/Flot.cs
+++ RemedyAPI_Example/Flot.cs
@@ -4,18 +4,18 @@ using System.Collections.Generic;
 namespace RemedyAPI_Example {
 
     public class FlotSeries {
-        public string Label;
-        public List<List<object>> Data;
-        public string Color;
+        public string label;
+        public List<List<object>> data;
+        public string color;
 
-        public FlotSeries( string label, Dictionary<DateTime, List<int>> data, int column, string colour = "#D9EDF7") {
-            Label = label;
-            Color = colour;
-            Data = new List<List<object>>();
+        public FlotSeries( string label, Dictionary<DateTime, List<int>> data, int column, string color = "#D9EDF7") {
+            this.label = label;
+            this.color = color;
+            this.data = new List<List<object>>();
             foreach ( var row in data ) {
                 var timestamp = row.Key.ToJavascriptTime();
                 var value = row.Value[ column - 1 ];
-                Data.Add( new List<object> { timestamp, value });
+                this.data.Add( new List<object> { timestamp, value });
             }
         }
     }
